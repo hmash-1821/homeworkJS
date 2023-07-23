e0b61c20364468674e8fbecf093aaa6c0574cd1a
lukatelia <telialuka123@gmail.com>
2023-07-24 00:20:05 +0400

    first commitjs

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+userName = 'Temo';
+lastName = "Maghlakelidze";
+userAge = '15';
+isGoaeli = true;
+
+console.log("I'm" + " " + userName + " " + lastName + " " + "Age" + " " + userAge + " , " + "Am i goa student = " + isGoaeli);
+
